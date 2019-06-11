18d5d70b9b35754ba9443def74dbfb9b5782422e
martin nievas <martin.nievas.ar@gmail.com>
2019-06-11 16:07:02 -0300

    Agrega resolución del ejercicio 3 de la guía de Selección

---
+++ 2019/junio11/ej3_v2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int a,b,c;
+
+
+  printf("Ingrese a: ");
+  scanf("%d", &a);
+  printf("Ingrese b: ");
+  scanf("%d", &b);
+  printf("Ingrese c: ");
+  scanf("%d", &c);
+
+  if (a > b) 
+    if (a > c) 
+      printf("El mayor es a\n");
+    else 
+      printf("El mayor es c\n");
+  else 
+    if (b > c) 
+      printf("El mayor es b\n");
+    else 
+      printf("El mayor es c\n");
+
+  return 0;
+}
