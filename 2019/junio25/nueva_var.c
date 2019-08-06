262ac10233e9d227913419598a1d36b0cd0b54b3
martin nievas <martin.nievas.ar@gmail.com>
2019-08-06 08:37:38 -0300

    Agrega ejemplo de variable local al for

---
+++ 2019/junio25/nueva_var.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i = 200;
+
+
+  printf("antes:%d\n", i);
+
+  for (int i = 0; i < 10; i++)
+    printf("%d\n", i);
+
+
+  printf("desp: %d\n", i);
+
+  return 0;
+}
