d84941f2f2970e215bc1869199600def93e1ecbd
martin nievas <martin.nievas.ar@gmail.com>
2020-05-28 11:19:35 -0300

    Agrega resolución del ejercicio 4 de la guía de selección

---
+++ 2020/mayo26/ejercicio4_seleccion.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n1, n2, r1;
+  float prom;
+
+  /** Ingreso las notas de los parciales */
+  printf("Ingrese la primera nota: ");
+  scanf("%d", &n1);
+  printf("Ingrese la segunda nota: ");
+  scanf("%d", &n2);
+
+  /** Determino si tiene que recursar */
+  if (n1<6) {
+    printf("Ingrese la nota del recupeartorio: ");
+    scanf("%d", &r1);
+  }
+
+  if (n2<6) {
+    printf("Ingrese la nota del recupeartorio: ");
+    scanf("%d", &r1);
+  }
+
+  /** Calculo el promedio */
+  /** Con llaves */
+  if (n1<6) {
+    prom = (n1 + n2 + r1)/3.0;
+  } else if (n2<6) {
+    prom = (n1 + n2 + r1)/3.0;
+  } else {
+    prom = (n1 + n2)/2.0;
+  }
+
+  /** Imprimo el estado académico */
+  /** Sin llaves */
+  if (prom < 6)
+    printf("Desaprobado\n");
+  else if (prom >= 8)
+    printf("Promocionado\n");
+  else
+    printf("Aprobado\n");
+
+  return 0;
+}
