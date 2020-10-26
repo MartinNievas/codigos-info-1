0d8490b3ae3950731546058f744cac02d1a4957b
martin nievas <martin.nievas.ar@gmail.com>
2020-10-26 11:22:44 -0300

    Agrega ejercicio visto en la clase de consulta del lunes

--- 2020/oct21/code/practica1.c
+++ 2020/oct21/code/practica1.c
@@ -21,18 +21,65 @@ typedef struct {
 
 int main(void) {
 
-  // Convertir de acceso secuencial -> acceso aleatorio
+  FILE *file1;
+  FILE *file2;
 
-  int id_buscar;
-  printf("Ingrese el id del producto a buscar\nIngrese -1 para salir\nid: ");
-  scanf("%d", &id_buscar);
-  while (id_buscar != -1){
-    // LLamado a la función imprimir_precio
+  file1 = fopen("precios.csv", "r");
+  file2 = fopen("precios.dat", "wb+");
+
+  // Convertir de acceso secuencial -> acceso aleatorio
+  if (file1 == NULL && file2 == NULL){
+    printf("ERROR\n");
+  } else {
+
+    producto_t producto;
+
+    while (!feof(file1)){
+      fscanf(file1, "%d,%f,%s", &producto.id
+          , &producto.precio
+          , producto.nombre);
+
+      fseek(file2, producto.id*sizeof(producto_t), SEEK_SET);
+      fwrite(&producto, sizeof(producto_t), 1, file2);
+    }
+
+
+    int id_buscar;
 
 
     printf("Ingrese el id del producto a buscar\nIngrese -1 para salir\nid: ");
     scanf("%d", &id_buscar);
+
+    while (id_buscar != -1){
+
+      imprimir_precio(file2, id_buscar);
+
+      printf("Ingrese el id del producto a buscar\nIngrese -1 para salir\nid: ");
+      scanf("%d", &id_buscar);
+
+    }
+
+    fclose(file1);
+    fclose(file2);
   }
 
   return 0;
+}
+
+void imprimir_precio(FILE *file, int id_producto){
+
+  producto_t producto;
+
+  fseek(file, id_producto*sizeof(producto_t), SEEK_SET);
+  fread(&producto, sizeof(producto_t), 1, file);
+
+  if(producto.id != 0){
+    printf("El producto existe\n");
+    printf("%d,%f,%s\n", producto.id
+        , producto.precio
+        , producto.nombre);
+  } else {
+    printf("El producto no existe\n");
+  }
+
 }
