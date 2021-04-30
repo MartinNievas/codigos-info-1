ff3e7eb09291d9488e69f670ce4502efeaa20dad
martin nievas <martin.nievas.ar@gmail.com>
2021-04-30 14:50:33 -0300

    Dudas
      - Agrega ejercicios resueltos de la guía
      - Respuesta de dudas y consultas

---
+++ 2021/abril27/problemas_guia_intro_c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int p, q, r;
+  scanf("%d", &p);
+
+  scanf("%d %d %d", &p, &q, &r);
+
+  printf("%d %d %d", p, q, r);
+
+  return 0;
+}
---
+++ 2021/abril27/programas_guia_intro_c.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+
+  //Definir las variables, “unavariable”, “p345”, “numero”.
+  int unavariable;
+  int p345;
+  int numero;
+
+  //Escribir un mensaje en pantalla,
+  //solicitando al usuario ingresar un número entero. Se debe
+  //imprimir la solicitud, en la cual al final debe incluir dos puntos y dejarse un espacio.
+  printf("Ingrese un número entero: ");
+
+//Solicitar al usuario ingresar un número entero y almacenarlo en una variable llamada “num”.
+  int num;
+  printf("Ingrese un número entero: ");
+  scanf("%d", &num);
+
+//Imprimir en pantalla el mensaje “Buen dı́a”.
+
+  printf("\"Buen día\"\n");
+
+
+  return 0;
+}
