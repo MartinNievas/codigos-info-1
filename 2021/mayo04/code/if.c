ba3febe767b0dd63a1fa08f52babe80e60e2d8a3
martin nievas <martin.nievas.ar@gmail.com>
2021-05-04 14:30:25 -0300

    Agrega ejemplo de if

---
+++ 2021/mayo04/code/if.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// En el libro D&D 8va, unidad 2.6
+
+int main(void) {
+
+  int numero = 10;
+
+  if (numero == 10){
+    printf("Número vale 10!\n");
+  }
+
+  return 0;
+}
---
+++ 2021/mayo04/code/if2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+  float temperatura = 1.2;
+
+  if (temperatura > 100){
+    printf("Encender ventilación\n");
+  }
+
+  return 0;
+}
