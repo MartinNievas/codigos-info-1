f3b0823b0361a7de27320dd20a02f7e9a4b92fc0
martin nievas <martin.nievas.ar@gmail.com>
2021-08-09 13:41:48 -0300

    Agrega el ejercicio resuelto en clase

--- 2021/agosto04/code/funciones_4.c
+++ 2021/agosto04/code/funciones_4.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define TAM 10 10 10
 //Funciones sin parámetros con valor de retorno
 
 int obtener_num_aleatorio(void)
 {
 
   int num;
-  srand(time(NULL));
 
-  // Devolver un número aleatorio entre 0 y 10
-  num = rand() % 11;
+  // Intervalo 23 - 100
+  num = rand() % 78 + 23;
 
   return num;
-
 }
 
 int main(void) {
 
-  printf("El número es: %d\n", obtener_num_aleatorio());
+  int val;
+
+  val = obtener_num_aleatorio();
+  printf("El número es: %d\n", val);
 
   return 0;
 }
