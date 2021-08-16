1aff9f7fe8b7a3417fcf187e61a3a5138712ec03
martin nievas <martin.nievas.ar@gmail.com>
2021-08-16 19:31:32 -0300

    Agrega programas para ver en clases

---
+++ 2021/agosto16/code/funcion7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int incrementar(void)
+{
+  static int a = 0;
+
+  return ++a;
+}
+
+int main(void)
+{
+
+  for(int i = 0; i < 10; i++)
+    printf("valor: %d\n", incrementar());
+
+  return 0;
+}
