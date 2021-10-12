e649e5c26505fdfeaeee6ebe3f11bede944318fd
martin nievas <martin.nievas.ar@gmail.com>
2021-10-12 14:55:00 -0300

    Agrega problemas para ver en clases

---
+++ 2021/oct12/code/analizar.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+// Datos de https://www.kaggle.com/tinnqn/precios-claros-precios-de-argentina/
+// Buscar el producto más caro y el más barato
+// Imprimir su nombre y el precio
+
+int main(void)
+{
+
+  FILE *fPtr;
+
+
+  fPtr = fopen("precios_parseados.csv", "r");
+
+  if (fPtr == NULL){
+    printf("El archivo no se abrió corréctamente\n");
+  } else {
+    float precio;
+    int id;
+    char nombre[20];
+
+    while (!feof(fPtr)){
+      fscanf(fPtr, "%f,%d,%[A-za-z ]\n", &precio, &id, nombre);
+      printf("%f,%d,%s\n", precio, id, nombre);
+    }
+
+  fclose(fPtr);
+
+
+
+  }
+
+  return 0;
+}
---
+++ 2021/oct12/code/escribir_archivo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+// Escribir en un archivo
+
+int main(void) {
+
+  FILE *cfPtr;
+
+  // Abrir el archivo
+  cfPtr = fopen("base_datos.txt", "a");
+
+  // Si no se abrió corréctamente, imprime el mensaje y termina
+  if ( cfPtr  == NULL) {
+    puts("El archivo no puede ser abierto\n");
+  } else {
+    printf("Archivo abierto\n");
+
+    char nombre[10];
+    char apellido[10];
+    int id;
+    printf("Ingrese el nombre apellido e id: ");
+    scanf("%10s%10s%d", nombre, apellido, &id);
+
+    while ( !feof(stdin) ) {
+
+      fprintf(cfPtr, "%s\t%s\t%d\n", nombre, apellido, id);
+      printf("Ingrese el nombre apellido e id: ");
+      scanf("%10s%10s%d", nombre, apellido, &id);
+
+    }
+    fclose(cfPtr);
+  }
+}
+
+
+
