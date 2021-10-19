8b8b971b83b758991333b2ed5d9796f200a9ff7f
martin nievas <martin.nievas.ar@gmail.com>
2021-10-19 14:02:16 -0300

    Agrega ejercicios para ver en clases

---
+++ 2021/oct19/code/acceso_aleatorio_leer.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define TAM 4
+// Antes de correr este programa, tiene que estar creado el archivo
+// lista_binaria.dat mediante el programa acceso_aleatorio2.c
+
+typedef struct{
+  char nombre[20];
+  long int id;
+  float precio;
+}prod_t;
+
+int main(void)
+{
+
+  prod_t producto;
+  FILE *fPtr;
+  int pos = 0;
+
+  fPtr = fopen("lista_binaria.dat", "rb");
+
+  if (fPtr == NULL){
+    printf("Problemas para abrir el archivo\n");
+  }  else {
+    printf("El archivo se pudo abrir de forma correcta\n");
+
+    while (!feof(fPtr)) {
+
+      fread(&producto, sizeof(prod_t), 1, fPtr);
+      if(producto.id != 0){
+        printf("nombre: %s\n", producto.nombre);
+        printf("precio: %f\n", producto.precio);
+        printf("id: %ld\n", producto.id);
+      }
+    }
+  }
+  fclose(fPtr);
+
+
+  return 0;
+}
---
+++ 2021/oct19/code/luz_teclado_flush.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+// OjO, tenemos que correr con sudo el programa
+
+int main() {
+
+  // El nombre del archivo varía con cada computadora
+  char archivo[100] = "/sys/class/leds/input56::capslock/brightness";
+  FILE *fPtr = fopen(archivo, "w");
+  if (fPtr == NULL) {
+    printf("No se puede abrir el archivo.\n");
+    exit(1);
+  } else {
+
+    printf("Archivo abierto.\n");
+
+    for (int i = 0; i < 100; i++) {
+      printf("Prende\n");
+      fprintf(fPtr, "%d", 1);
+      sleep(1);
+      fflush(fPtr);
+      printf("Apaga\n");
+      fprintf(fPtr, "%d", 0);
+      sleep(1);
+      fflush(fPtr);
+    }
+
+  }
+
+  fclose(fPtr);
+
+  return 0;
+}
