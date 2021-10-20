9bd992ecd380e17b49ad61e806b502767f0aa98f
martin nievas <martin.nievas.ar@gmail.com>
2021-10-20 19:51:22 -0300

    Agrega ejercicios y códigos para analizar en clclase

---
+++ 2021/oct20/code/acceso_aleatorio0.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define TAM 3
+
+typedef struct{
+  char nombre[20];
+  long int id;
+  float precio;
+}prod_t;
+
+int main(void)
+{
+
+  prod_t producto;
+  FILE *fPtr;
+
+  fPtr = fopen("lista_binaria.dat", "wb");
+
+  if (fPtr == NULL){
+    printf("Problemas para abrir el archivo\n");
+  }  else {
+    printf("El archivo se pudo abrir de forma correcta\n");
+
+
+
+    producto.id = 0;
+    fseek(fPtr, producto.id* sizeof(prod_t), SEEK_SET);
+    fwrite(&producto, sizeof(prod_t), 1, fPtr);
+    for ( int i = 0;  i < TAM;  i++){
+      printf("Ingrese el nombre del producto: ");
+      scanf("%s", producto.nombre);
+      printf("Ingrese el id: ");
+      scanf("%ld", &producto.id);
+      printf("Ingrese el precio: ");
+      scanf("%f", &producto.precio);
+
+      fseek(fPtr, producto.id* sizeof(prod_t), SEEK_SET);
+      fwrite(&producto, sizeof(prod_t), 1, fPtr);
+    }
+    fclose(fPtr);
+  }
+
+  return 0;
+}
