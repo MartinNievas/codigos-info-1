2bff1c2a98f5b207f059f56bd5d16eda73d201c2
Martin Nievas <martin.nievas.ar@gmail.com>
2022-08-09 15:26:36 -0300

    Agrega ejercicios para resolver en clase

---
+++ 2022/agosto09/code/ejercicio1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+// Implementar una función que imprima un número aleatorio
+// entre -10 y 10 incluyendo los extremos
+
+int obtener_num_aleatorio(void)
+{
+  // Completar
+  return -11;
+}
+
+int main(void) {
+
+  int num;
+  srand(time(NULL));
+
+  printf("Número aleatorio: %d\n", obtener_num_aleatorio());
+
+  return 0;
+}
