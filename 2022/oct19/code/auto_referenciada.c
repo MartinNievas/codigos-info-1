38001d1525bc0092351f79a742b34c833ad718db
Martin Nievas <martin.nievas.ar@gmail.com>
2022-10-19 18:19:24 -0300

    Agrega códigos para analizar en lcases

---
+++ 2022/oct19/code/auto_referenciada.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct producto {
+  float precio;
+  char nombre[40];
+
+  // struct producto taza; // ERROR
+  struct producto* pproducto; // Si se puede
+};
+
+int main(void) {
+
+  struct producto limpieza[10];
+
+
+  return 0;
+}
