4f8bda90586d7a5486c00b84385dfe04b560b872
Martin Nievas <martin.nievas.ar@gmail.com>
2023-05-11 08:36:05 -0300

    Agrega programa de suma

---
+++ 2023/may05/suma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int num1;
+  int num2;
+  printf("Ingrese el primer número: ");
+  scanf("%d", &num1);
+  printf("Ingrese el segundo número: ");
+  scanf("%d", &num2);
+
+  printf("La suma es %d\n", num1+num2);
+
+  return 0;
+}
