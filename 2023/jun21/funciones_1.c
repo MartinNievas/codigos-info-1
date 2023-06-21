b8ff5c8f46c5d8f8f6c4f939fb08fb1520eb3d33
Martin Nievas <martin.nievas.ar@gmail.com>
2023-06-21 20:47:41 -0300

    Agrega programas para analizar en clase

---
+++ 2023/jun21/funciones_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void saludo(void)
+{
+  printf("Hola!\n");
+}
+
+int main(void)
+{
+
+  saludo();
+
+  return 0;
+}
---
+++ 2023/jun21/funciones_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int ingresar_opcion(void)
+{
+  int op;
+
+  printf("1) Suma\n2) Resta\n3) Salir\n");
+  printf("Ingrese una opción: ");
+  scanf("%d", &op);
+
+  return op;
+}
+
+int main(void)
+{
+  int opcion;
+
+  /* Llamado a la función */
+  opcion = ingresar_opcion();
+
+  printf("Usted ingresó la opción: %d\n", opcion);
+
+  return 0;
+}
