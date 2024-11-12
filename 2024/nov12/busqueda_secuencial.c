88b6eb263bd856972bcf426314bf3315a5c5217b
Martin Nievas <martin.nievas.ar@gmail.com>
2024-11-12 13:28:48 -0300

    Agrega programas para analizar en clases

---
+++ 2024/nov12/busqueda_secuencial.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define TAM 10
+
+// Secuencial
+int main(void) {
+  int arreglo[TAM];
+
+  for(int i = 0; i < TAM; i++){
+    printf("Ingrese los numeros del arreglo:");
+    scanf("%d",&arreglo[i]);
+  }
+  int buscado = 0;
+  int encontrado = 0;
+  printf("Que numero desea buscar?:");
+  scanf("%d",&buscado);
+
+  for(int j = 0; j < TAM; j++){
+    if(buscado == arreglo[j]){
+      encontrado = j;
+    }
+  }
+
+  if(encontrado != 0){
+    printf("Su numero se encuentra en la posicion nuero %d del arreglo",(encontrado + 1));
+  }else
+    printf("El numero que desea buscar no existe dentro del arreglo");
+
+
+  return 0;
+}
---
+++ 2024/nov12/performance.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+// OpenMP
+#include <omp.h>
+#define TAM 1<<10
+
+// Tarea opcional mover todo esto a una biblioteca separada
+// Para compilar:
+// !clear && gcc -Wall -std=c99 % -lm -lgomp && ./a.out
+
+
+/** Función para inicializar tam elementos de un arreglo a partir de
+  * la dirección del primer elemento. Se inicializan de forma
+  * aleatoria
+  *
+  * Parámetros
+  *
+  * a: dirección del primer elemento
+  * tam: cantidad de elementos a inicializar
+  * */
+void inicializa_arreglo(int *a, int tam){
+
+  printf("Inicializando con números aleatorios\n");
+  for (int i = 0; i < tam; i++)
+    *(a+i) = rand() % 100000;
+
+}
+
+
+/** Intercambia dos elementos a partir de sus direcciones de memoria
+  * en caso que el primero sea menor que el segundo
+  *
+  * Parámetros
+  * a: dirección del primer elemento
+  * b: dirección del segundo elemento
+  *
+  *  */
+void swap_menor(int *a, int *b)
+{
+  int temp;
+  if (*a < *b){
+    temp = *a;
+    *a = *b;
+    *b = temp;
+  }
+}
+
+/** Método de ordenamiento burbuja
+  *
+  * Parámetros
+  * a: dirección del primer elemento del arreglo
+  * tam: cantidad de elementos en el arreglo
+  *  */
+void ordena_arreglo_burbuja(int *a, int tam)
+{
+  int i, j;
+  for (i = 0; i < tam; i++){
+    for (j = 0; j < tam-1; j++){
+      swap_menor(&a[j], &a[j+1]);
+    }
+  }
+}
+
+/** Método de ordenamiento burbuja mejorado
+  *
+  * Parámetros
+  * a: dirección del primer elemento del arreglo
+  * tam: cantidad de elementos en el arreglo
+  *  */
+void ordena_arreglo_burbuja_mejorado(int *a, int tam)
+{
+  int i, j;
+  for (i = 0; i < tam; i++){
+    for (j = 0; j < tam - 1 - i; j++){
+      swap_menor(&a[j], &a[j+1]);
+    }
+  }
+}
+
+/** Función para imprimir los primeros tam elementos de un arreglo
+  * a partir de la dirección del primer elemento
+  *
+  * Parámetros
+  * a: dirección del primer elemento
+  * tam: cantidad de elementos a imprimir
+  *  */
+void imprime_arreglo(int *a, int tam){
+  for (int i = 0; i < tam; i++){
+    printf("%d\n", a[i]);
+  }
+}
+
+
+/** Función de comparación necesaria para la función qsort */
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+/** Función principal */
+int main(void)
+{
+
+  int arreglo[TAM];
+  int tam;
+  float start, end;
+
+  tam = TAM;
+  printf("%ld elementos\n", TAM);
+
+  srand(time(NULL));
+
+  //for (int i = 0; i < 3; i++){
+  //  inicializa_arreglo(arreglo, tam);
+  //  start = omp_get_wtime();
+  //    ordena_arreglo_burbuja(arreglo, tam);
+  //  end = omp_get_wtime();
+  //  printf("ordena burbuja: %f\n", end-start);
+  //}
+
+  //for (int i = 0; i < 3; i++){
+  //  inicializa_arreglo(arreglo, tam);
+  //  start = omp_get_wtime();
+  //    ordena_arreglo_burbuja_mejorado(arreglo, tam);
+  //  end = omp_get_wtime();
+  //  printf("ordena burbuja mejorado: %f\n", end-start);
+  //}
+
+
+  for (int i = 0; i < 3; i++){
+    inicializa_arreglo(arreglo, tam);
+    start = omp_get_wtime();
+      qsort(arreglo, tam, sizeof(int), cmpfunc);
+    end = omp_get_wtime();
+    printf("ordena qsort: %.23f\n", end-start);
+  }
+
+  /** imprime_arreglo(arreglo,100); */
+
+  return 0;
+}
