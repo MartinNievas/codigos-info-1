426ee19f6cffb3f4dc3428a87ac3b5f29210b85c
Martin Nievas <martin.nievas.ar@gmail.com>
2025-06-03 17:08:20 -0300

    Agrega programa analizado en clase

---
+++ 2025/may07/float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+  float num1 = 1.0/3.0;
+  double num2 = 1.0/3.0;
+
+  printf("float: %.20f\n", num1);
+  printf("double: %.20lf\n", num2);
+
+  return 0;
+}
